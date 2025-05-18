38f6754ba125bb50f14395ba5e0c699a8c1eff27
DipstickPinez <dipstckpinez@gmail.com>
2025-05-18 07:54:47 -0700

    Added basic page functionality

--- source/pullpage/pack.js
+++ source/pullpage/pack.js
@@ -27,3 +27,32 @@ function addCurrencyToDocument() {
         packs[i].innerHTML = `Packs: ${packsValue}`;
     }
 }
+
+function populateLocalStorage() {
+    localStorage.clear();
+    localStorage.setItem('Gems', JSON.stringify(50)); // Add some gems
+    localStorage.setItem('Packs', JSON.stringify(3)); // Add some packs
+    for (let i = 0; i < 30; i++) {
+        let card = {
+            "name": `card${i}`,
+            "rarity": i % 5,
+            "acquisition": Date.now() - ((i % 7) * 1000 * 60 * 60 * 24)
+        }
+        addCardToCollection(card);
+    }
+}
+
+function verifyPullCount() {
+    const pull1 = document.getElementsByClassName("pull1");
+    const pull10 = document.getElementsByClassName("pull10");
+
+    const packsValue = localStorage.getItem("Packs");
+
+    for (let i = 0; i < pull1.length; i++) {
+        pull1[i].classList.toggle("not-enough-packs", packsValue < 1);
+    }
+
+    for (let i = 0; i < pull10.length; i++) {
+        pull10[i].classList.toggle("not-enough-packs", packsValue < 10);
+    }
+}
