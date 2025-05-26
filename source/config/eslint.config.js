542ddf2ade6180e72558791175ba3f1e039c8d77
Chakshan <chakshan.kothakota@gmail.com>
2025-05-25 20:41:44 -0700

    prettier format and unit test enable

--- source/config/eslint.config.js
+++ source/config/eslint.config.js
@@ -2,23 +2,29 @@
 import js from '@eslint/js';
 import prettier from 'eslint-config-prettier';
 import globals from 'globals';
+import jest from 'eslint-plugin-jest';
 
 export default [
   js.configs.recommended,
   prettier,
   {
     files: ['**/*.js'],
+    plugins: {
+      jest,
+    },
     languageOptions: {
       ecmaVersion: 'latest',
       sourceType: 'module',
       globals: {
         ...globals.browser,
+        ...jest.environments.globals.globals,
       },
     },
     rules: {
       // Add custom rules here
       'no-unused-vars': 'warn',
       'no-console': 'off',
+      ...jest.configs.recommended.rules,
     },
   },
 ];
--- source/util/utils.js
+++ source/util/utils.js
@@ -1,15 +1,14 @@
 export function getCollectionCards() {
-
-	if (localStorage.getItem('Collection')) {
-		return JSON.parse(localStorage.getItem('Collection'));
-	}
-	return [];
+  if (localStorage.getItem('Collection')) {
+    return JSON.parse(localStorage.getItem('Collection'));
+  }
+  return [];
 }
 
 export function addCardToCollection(card) {
-	if (!card) return;
-	// card.acquisition = Date.now();
-	const cards = getCollectionCards();
-	cards.push(card);
-	localStorage.setItem('Collection', JSON.stringify(cards));
+  if (!card) return;
+  // card.acquisition = Date.now();
+  const cards = getCollectionCards();
+  cards.push(card);
+  localStorage.setItem('Collection', JSON.stringify(cards));
 }
