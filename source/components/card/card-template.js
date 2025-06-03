c400c1c41cc3dd5e1fb03f48f533a552bdcf789f
JoshCaneday <146874169+JoshCaneday@users.noreply.github.com>
2025-06-03 13:12:36 -0700

    finished adding cards

--- source/components/card/card-template.js
+++ source/components/card/card-template.js
@@ -7,48 +7,48 @@ export const template = (data) => {
 	let opacity = 0;
 
 	if (data.rarity == 1) {
-		blur = 0;
-		spread = 0;
-		R = 0;
-		G = 0;
-		B = 0;
-		opacity = 0;
+		blur = 10;
+		spread = 0.5;
+		R = 255;
+		G = 255;
+		B = 255;
+		opacity = 0.7;
 	}
 	else if (data.rarity == 2) {
-		blur = 0;
-		spread = 0;
-		R = 0;
-		G = 0;
+		blur = 10;
+		spread = 1;
+		R = 89;
+		G = 255;
 		B = 0;
-		opacity = 0;
+		opacity = 0.9;
 	}
 	else if (data.rarity == 3) {
-		blur = 0;
-		spread = 0;
+		blur = 12;
+		spread = 3;
 		R = 0;
-		G = 0;
-		B = 0;
-		opacity = 0;
+		G = 38;
+		B = 255;
+		opacity = 1;
 	}
 	else if (data.rarity == 4) {
-		blur = 0;
-		spread = 0;
-		R = 0;
+		blur = 15;
+		spread = 5;
+		R = 170;
 		G = 0;
-		B = 0;
-		opacity = 0;
+		B = 255;
+		opacity = 1;
 	}
 	else if (data.rarity == 5) {
-		blur = 0;
-		spread = 0;
-		R = 0;
-		G = 0;
+		blur = 18;
+		spread = 7;
+		R = 255;
+		G = 174;
 		B = 0;
-		opacity = 0;
+		opacity = 1;
 	}
 
 	return `
-		<div class="card" style="background-image: url('../card_data/images/${data.image}'), url('../card_data/images/heap.png'); box-shadow: 0 0 10px 0.5px rgba(255, 255, 255, 1);">
+		<div class="card" style="background-image: url('../card_data/images/${data.image}'), url('../card_data/images/Heap.png'); box-shadow: 0 0 ${blur}px ${spread}px rgb(${R}, ${G}, ${B}, ${opacity}) inset;">
 			<p class="name"> ${data.name} </p>
 			<p class="rarity"> ${'★'.repeat(data.rarity)} </p>
 			<div class="stats">
