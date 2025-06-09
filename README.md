24d8de9178e448f8497cf4f99a407a7eb2a0790a
Chakshan <Chakshan@users.noreply.github.com>
2025-06-09 00:01:35 +0000

    deploy: f311f07798359b5ed477cd9baceef28ac121d354

--- README.md
+++
@@ -1,6 +0,0 @@
-# cse110-sp25-group6
-
-## About us
-Read this [page](https://github.com/cse110-sp25-group6/cse110-sp25-group6/blob/main/admin/team.md) to learn more about us! 
-<br>
-Check out our First Sprint Results: [Status Video 1](https://youtu.be/zg7NDSaTIAg)
---
+++ components/top-bar/top-bar.js
@@ -0,0 +1,42 @@
+/**
+ *
+ * Defines a custom <top-bar> element that displays the page title and currency values.
+ * Retrieves currency from local storage and uses a shadow DOM to encapsulate its styles.
+ * 
+ */
+
+import { styles } from './top-bar-styles.js';
+import { template } from './top-bar-template.js';
+
+class TopBar extends HTMLElement {
+	/**
+	 * Attaches shadow DOM and sets default data.
+	 */
+	constructor() {
+		super();
+		this.attachShadow({ mode: 'open' });
+		this.data = { pageTitle: "", gems: 0, packs: 0 };
+	}
+
+	/**
+	 * Called when element is connected. Renders the component.
+	 */
+	connectedCallback() {
+		this.render();
+	}
+
+	/**
+	 * Renders the top bar by updating data from dataset and local storage, then injecting the template.
+	 */
+	render() {
+		this.data.pageTitle = this.dataset.title || "";
+		this.data.gems = localStorage.getItem('Gems') || 0;
+		this.data.packs = localStorage.getItem('Packs') || 0;
+		this.shadowRoot.innerHTML = `
+            <style>${styles}</style>
+            ${template(this.data)}
+        `;
+	}
+}
+
+customElements.define('top-bar', TopBar);
